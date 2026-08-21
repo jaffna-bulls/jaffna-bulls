1c34050d36fee321d0b53cf59aee176705971814
Shahanshaeek <shahanshaeek@advicelab.com.au>
2026-08-21 15:41:48 +0530

    News new page initiated

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
   const { pathname, hash } = useLocation();
@@ -14,7 +14,11 @@ export default function ScrollToTop() {
       }
     }
 
-    window.scrollTo({ top: 0, left: 0, behavior: 'instant' in window ? 'instant' : 'auto' });
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant" in window ? "instant" : "auto",
+    });
   }, [pathname, hash]);
 
   return null;
